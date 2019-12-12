cc032a8f9b2afa6f7892f94cd53c6e914fa3cc35
Paul Vallis <paul.vallis@mudano.com>
2019-12-12 16:41:04 +0000

    Day 4, pt 2

--- day-four.rb
+++ day-four.rb
@@ -1,8 +1,8 @@
 class DayFour
   @@data = (240920..789857).to_a
 
-  @@hasRepeatingChar = -> (num) { num.to_s.match(/1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,}|0{2,}/) != nil }
-  @@hasOneDouble = -> (num) { num.to_s.scan(/1{2,2}|2{2,2}|3{2,2}|4{2,2}|5{2,2}|6{2,2}|7{2,2}|8{2,2}|9{2,2}|0{2,2}/).count == 1 }
+  @@hasRepeatingChar = -> (num) { num.to_s.match(/1{2,2}|2{2,2}|3{2,2}|4{2,2}|5{2,2}|6{2,2}|7{2,2}|8{2,2}|9{2,2}|0{2,2}/) != nil }
+  @@hasOneDouble = -> (num) { num.to_s.scan(/1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,}|0{2,}/).any? { |match| match.length == 2 } }
   @@valuesIncrease = -> (num) { num.to_s.split('').reduce([true, '0']) { | p, c | [p[0] && c.to_i >= p[1].to_i, c] }[0] }
 
   def first  
@@ -13,11 +13,9 @@ class DayFour
   end
 
   def second
-    puts 11122.to_s.scan(/1{2,2}|2{2,2}|3{2,2}|4{2,2}|5{2,2}|6{2,2}|7{2,2}|8{2,2}|9{2,2}|0{2,2}/).count
-
-    # puts @@data
-    #   .select(&@@valuesIncrease)
-    #   .select(&@@hasOneDouble)
-    #   .count
+    puts @@data
+      .select(&@@valuesIncrease)
+      .select(&@@hasOneDouble)
+      .count
   end
 end
