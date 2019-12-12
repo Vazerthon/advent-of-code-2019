7c138fcb27051cf03c661b20531370c1c68d870c
Paul Vallis <paul.vallis@mudano.com>
2019-12-12 16:20:48 +0000

    Day 4, pt1

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require_relative 'day-one'
 require_relative 'day-two'
 require_relative 'day-three'
+require_relative 'day-four'
 
 puts "Day number..."
 day = {
     1 => [DayOne.new, "one"],
     2 => [DayTwo.new, "two"],
     3 => [DayThree.new, "three"],
+    4 => [DayFour.new, "four"],
 }[gets.to_i]
 
 puts "Part number..."
---
+++ day-four.rb
@@ -0,0 +1,23 @@
+class DayFour
+  @@data = (240920..789857).to_a
+
+  @@hasRepeatingChar = -> (num) { num.to_s.match(/1{2,}|2{2,}|3{2,}|4{2,}|5{2,}|6{2,}|7{2,}|8{2,}|9{2,}|0{2,}/) != nil }
+  @@hasOneDouble = -> (num) { num.to_s.scan(/1{2,2}|2{2,2}|3{2,2}|4{2,2}|5{2,2}|6{2,2}|7{2,2}|8{2,2}|9{2,2}|0{2,2}/).count == 1 }
+  @@valuesIncrease = -> (num) { num.to_s.split('').reduce([true, '0']) { | p, c | [p[0] && c.to_i >= p[1].to_i, c] }[0] }
+
+  def first  
+    puts @@data
+      .select(&@@hasRepeatingChar)
+      .select(&@@valuesIncrease)
+      .count
+  end
+
+  def second
+    puts 11122.to_s.scan(/1{2,2}|2{2,2}|3{2,2}|4{2,2}|5{2,2}|6{2,2}|7{2,2}|8{2,2}|9{2,2}|0{2,2}/).count
+
+    # puts @@data
+    #   .select(&@@valuesIncrease)
+    #   .select(&@@hasOneDouble)
+    #   .count
+  end
+end
